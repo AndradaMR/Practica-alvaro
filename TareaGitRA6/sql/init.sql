dd1addadaf10afc5497a9f87516b987825cf954a
alumno <lauritabasurtoteno@gmail.com>
2026-02-17 11:55:51 +0100

    Arreglado archivo  init.sql

--- TareaGitRA6/sql/init.sql
+++ TareaGitRA6/sql/init.sql
@@ -1,7 +1,7 @@
 -- TICKET A: Error de sintaxis en la definición de la tabla
 CREATE TABLE usuarios (
     id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(100) NOT NULL -- Aquí falta una coma
+    nombre VARCHAR(100) NOT NULL, -- Añadida coma faltante
     email VARCHAR(100) UNIQUE NOT NULL,
     creado_en TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
