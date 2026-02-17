0bcc905844b0e7b7b96909c7de1554f878e8cdc6
AndradaMR <andrada.robitu@gmail.com>
2026-02-17 11:04:47 +0100

    quitado el db.php de config y configuracion de gitignore

--- TareaGitRA6/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ERROR DE SEGURIDAD: Credenciales expuestas
-// El equipo debe eliminar este archivo del historial de Git
-define('DB_HOST', '192.168.1.100');
-define('DB_NAME', 'prod_intermodular');
-define('DB_USER', 'admin_root');
-define('DB_PASS', 'P@ssw0rd_Secret_2026!'); 
-
-$conexion = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
