88f0686a358d1058667d3d41ede860c0fa550c25
AndradaMR <andrada.robitu@gmail.com>
2026-02-19 09:55:27 +0100

    añadidos los nombres al index.php

--- TareaGitRA6/public/index.php
+++ TareaGitRA6/public/index.php
@@ -6,3 +6,8 @@ echo "<p>Estado del sistema: <span style='color:red'>Sin estilos</span></p>";
 // El Alumno B arreglará el JS que se carga aquí:
 ?>
 <script src="js/main.js"></script>
+
+//Andrada María Robitu
+//Cristina Gonzalez Ortas
+//Laura Basurto Teno
+//Fernando Chavez 
